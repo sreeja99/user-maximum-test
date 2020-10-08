82b75fdd0e7d843cf746e0cbc29a110ea09e76e2
sreeja99 <srijagodishala@gmail.com>
2020-10-08 22:26:12 +0530

    Refactored:USed Generic Method for Checking Maximum Values

--- usermaximum/UserMaximum.java
+++ usermaximum/UserMaximum.java
@@ -3,28 +3,28 @@ package com.usermaximum;
 import java.util.Scanner;
 
 public class UserMaximum {
-		//method for finding maximum String
-	public static String findMaximum(String x,String y,String z) {
-		String max = x;
-		if(y.compareTo(x)>0)
-			max =y;
-		if(z.compareTo(x)>0)
-			max =z;
-		return max;
-	}
+	//method for finding maximum value
+	public static <E extends Comparable<E>> E findMaximum(E x,E y,E z) {
+			E max = x;
+			if(y.compareTo(max)>0)
+				max =y;
+			if(z.compareTo(max)>0)
+				max =z;
+			return max;
+		}
 
 	public static void main(String[] args) {
 		System.out.println("Welcome");
 		Scanner sc=new Scanner(System.in);
 		//taking input
-		System.out.println("Enter the first string:");
+		System.out.println("Enter the first value:");
 		String x= sc.nextLine();
-		System.out.println("Enter the second string:");
+		System.out.println("Enter the second value:");
 		String y= sc.nextLine();
-		System.out.println("Enter the third string:");
+		System.out.println("Enter the third value:");
 		String z= sc.nextLine();
-		//printing highest String
-		System.out.println("Maximum String"+findMaximum(x,y,z));
+		//printing highest value
+		System.out.println("Maximum Value:"+findMaximum(x,y,z));
 	}
 
 }
--- usermaximum/UserMaximumTest.java
+++ usermaximum/UserMaximumTest.java
@@ -7,21 +7,52 @@ import org.junit.jupiter.api.Test;
 import junit.framework.Assert;
 
 class UserMaximumTest {
+	@Test
+	void CheckMaxIntegerAtFirstPosition() {
+		int max =UserMaximum.findMaximum(8,4,7);
+		Assert.assertEquals(8,max);
+	}
+	@Test
+	void CheckMaxIntegerAtSecondPosition() {
+		int max =UserMaximum.findMaximum(8,9,7);
+		Assert.assertEquals(9,max);
+	}
+	@Test
+	void CheckMaxIntegerAtThirdPosition() {
+		int max =UserMaximum.findMaximum(7,4,8);
+		Assert.assertEquals(8,max);
+	}
+	@Test
+	void CheckMaxFloatAtFirstPosition() {
+		float max =UserMaximum.findMaximum(8.3f,4.4f,7.1f);
+		Assert.assertEquals(8.3f,max);
+	}
+	@Test
+	void CheckMaxFloatAtSecondPosition() {
+		float max =UserMaximum.findMaximum(8.1f,9.4f,7.3f);
+		Assert.assertEquals(9.4f,max);
+	}
+	@Test
+	void CheckMaxFloatAtThirdPosition() {
+		float max =UserMaximum.findMaximum(7.3f,4.5f,8.5f);
+		Assert.assertEquals(8.5f,max);
+	}
+	@Test
+	public void CheckMaxStringAtFirstPosition1() {
+		String max = UserMaximum.findMaximum("Peach", "Apple", "Banana");
+		assertEquals("Peach", max);
+	}
 
 	@Test
-	void CheckMaxStringAtFirstPosition() {
-		String max =UserMaximum.findMaximum("Peach","Apple","Banana");
-		Assert.assertEquals("peach",max);
+	public void CheckMaxStringAtSecondPosition() {
+		String max = UserMaximum.findMaximum("Apple", "Peach", "Banana");
+		assertEquals("Peach", max);
 	}
+
 	@Test
-	void CheckMaxStringAtSecondPosition() {
-		String max =UserMaximum.findMaximum("Apple", "Peach","Banana");
-		Assert.assertEquals("peach",max);
-	}
-	@Test
-	void CheckMaxStringAtThirdPosition() {
-		String max =UserMaximum.findMaximum("Apple","Banana", "Peach");
-		Assert.assertEquals("peach",max);
+	public void CheckMaxStringAtThirdPosition() {
+		String max = UserMaximum.findMaximum("Apple", "Banana", "Peach");
+		assertEquals("Peach", max);
 	}
 
 }
